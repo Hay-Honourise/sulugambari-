794d9618ef239799052bf3968956f671c3f17eb4
Razaq Hammed Abiola <hayhonourise2016@gmail.com>
2024-06-20 02:16:26 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ $(".testimonials-carousel").owlCarousel({
     autoplay: true,
     autoplayTimeout: 45000, // Set to 45,000 milliseconds (45 seconds)
     dots: true,
-    loop: true,
+    loop: false,
     responsive: {
         0:{
             items:1
@@ -105,22 +105,5 @@ $(".testimonials-carousel").owlCarousel({
     });
     
 })(jQuery);
-
-
-function showMessageBox(abdulrazaq) {
-    alert("• Abubakar Sulu-Gambari, SAN, LLB, BL, LLM, MCIArb, the Principal Partner was called to the Bar in 1991.\n" +
-          "• He holds an LL.M. degree in Shipping and Maritime Law from the University College London (UCL) and specializes in all aspects of maritime litigation, charter party negotiations, ship financing and shipping arbitration.\n" +
-          "• He has worked with various prestigious law firms in Nigeria and abroad including Kehinde Sofola & Co, Femi Atoyebi & Co. and Stanbrook & Hooper (now McDermott Will Emery/Stanbrook LLP) in Brussels.\n" +
-          "• He has worked with various prestigious law firms in Nigeria and abroad including Kehinde Sofola & Co, Femi Atoyebi & Co. and Stanbrook & Hooper (now McDermott Will Emery/Stanbrook LLP) in Brussels.\n" +
-          "• Abubakar headed a team that worked on a core LNG project. He is currently heading the team working on the AKK Gas Pipeline Project and has extensive experience in litigation in the Maritime and the Oil and Gas Industries amongst others. He is a member of the Chartered Institute of Arbitrators.");
-}
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 392d19bea12f395d9b0663dde53a7707ff39cfe8
-  
-
-
 
 
